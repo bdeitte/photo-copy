e9a8c86435d329311c675cf5c93335fd9fc2f03f
Brian Deitte <bdeitte@gmail.com>
2026-03-04 19:35:23 -0600

    feat: initialize Go project with cobra root command and --debug flag

---
+++ cmd/photo-copy/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/briandeitte/photo-copy/internal/cli"
+
+func main() {
+	cli.Execute()
+}
---
+++ internal/cli/root.go
@@ -0,0 +1,29 @@
+package cli
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var debug bool
+
+func NewRootCmd() *cobra.Command {
+	rootCmd := &cobra.Command{
+		Use:   "photo-copy",
+		Short: "Copy photos and videos between Flickr, Google Photos, S3, and local directories",
+	}
+
+	rootCmd.PersistentFlags().BoolVar(&debug, "debug", false, "Enable verbose debug logging to stderr")
+
+	return rootCmd
+}
+
+func Execute() {
+	rootCmd := NewRootCmd()
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
